c7c3d073418d58b7c7b8ddda2f2a4814c22fb0e2
Rex Dieter <rdieter@fedoraproject.org>
2011-08-23 08:35:10 -0500

    hopefully fix ftbfs openssl issues once and for all (sf bug #3027341)

--- pith/smkeys.c
+++ pith/smkeys.c
@@ -277,7 +277,7 @@ char *
 get_x509_subject_email(X509 *x)
 {
     char *result = NULL;
-#if OPENSSL_VERSION_NUMBER >= 0x1000000f /* OpenSSL 1.0.0 */
+#if OPENSSL_VERSION_NUMBER >= 0x10000000L /* OpenSSL 1.0.0 */
     STACK_OF(OPENSSL_STRING) *emails;
 #else /* OpenSSL 0.x and 1.0.0-dev/beta */
     STACK *emails;
